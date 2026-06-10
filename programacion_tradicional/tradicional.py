39b8c5b3d4b28d598ca29d191d16acf879b4789e
CarlosPereira-2006 <cp148486@gmail.com>
2026-06-10 16:58:04 -0500

    Add files via upload

---
+++ programacion_tradicional/tradicional.py
@@ -0,0 +1,20 @@
+# Registrar la mascota
+def registrar_mascota():
+    nombre = input("Ingrese el nombre de la mascota: ")
+    especie = input("Ingrese la especie de la mascota: ")
+    edad = int(input("Ingrese la edad de la mascota: "))
+
+    return nombre, especie, edad
+
+
+# Mostrar la información
+def mostrar_mascota(nombre, especie, edad):
+    print("\n--- INFORMACIÓN DE LA MASCOTA ---")
+    print(f"Nombre: {nombre}")
+    print(f"Especie: {especie}")
+    print(f"Edad: {edad} años")
+
+
+# Programa principal
+nombre, especie, edad = registrar_mascota()
+mostrar_mascota(nombre, especie, edad)
